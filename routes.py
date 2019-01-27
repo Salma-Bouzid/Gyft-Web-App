18a2b7afa5e1bce8533a2df1cb673c399c105924
Salma Bouzid <salmaa.bouzid@gmail.com>
2019-01-27 22:04:24 +0100

    Update routes.py

--- routes.py
+++ routes.py
@@ -259,4 +259,4 @@ def mysql_error(err):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
